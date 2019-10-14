79e519f3b470316e45c5f9587d05143fb61bbbb4
Plimon Howard V <plimon.howard@gmail.com>
2019-10-14 20:10:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arry)
+  rt_ary = Array.new
+  arry.each do |arry|
+    rt_ary << "Hello, my name is #{arry}."
+  return rt_ary
+end
+
+def assign_rooms
+
+end
+
+def printer
+
+end
